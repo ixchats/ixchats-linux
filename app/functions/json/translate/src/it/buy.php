17f05c198bc06e86b4bf0100bc50558b1b29f4dc
ErrorOliver <ErrorOliver6@proton.me>
2025-09-06 03:47:00 +0000

    Create buy.php

---
+++ app/functions/json/translate/src/it/buy.php
@@ -0,0 +1,250 @@
+<?php
+$array = [
+     'buyshort' => 'Compra un nome utente corto',
+     'notavailable' => 'Nome corto non disponibile.',
+     'terms' => 'Devi accettare i termini',
+     'nametaken' => 'Spiacente, nome già in uso',
+     'costchanged' => 'Il costo è cambiato',
+     'notfound' => 'non trovato',
+     'wrongpassword' => 'Password errata',
+     'noxats' => 'Non hai abbastanza xats',
+     'namechanged' => 'Nome cambiato correttamente',
+     ''.XAT_NAME.'name' => 'Nome utente di '.XAT_NAME.':',
+     'password' => 'Password:',
+     'desiredname' => 'Nome desiderato:',
+     'noteil' => 'Nota: la ’i’ sarà sempre minuscola. La ’L’ sarà sempre maiuscola.',
+     'iagree' => 'Ho letto e accetto le regole e i <a href="//'.DOMAIN.'/terms">termini</a>',
+     'getcost' => 'Ottieni costo',
+     'once' => 'Clicca solo una volta!',
+     'letters' => 'Il nome contiene lettere non ammesse',
+     'short' => 'Il nome è troppo corto',
+     'long' => 'Il nome è troppo lungo',
+     'notallowed' => 'Il nome non è ammesso. Si prega di provarne un altro.',
+     'firstletter' => 'La prima lettera non può essere un numero.',
+     'youcan' => 'Puoi usare le tue xats per comprare un nome utente corto (4-9 lettere).',
+     'rules' => 'Regole:',
+     'rulesname' => '<li>Devi <a href="//'.DOMAIN.'/terms">rispettare i termini</a></li>
+ <li>I costi dei nomi corti variano, premi Ottieni il costo per scoprirlo</li>
+ <li>Se il tuo nome corto è inutilizzato da più di 90 giorni potrebbe essere messo nuovamente in vendita.</li>
+ <li>È consentito solo un nome corto a persona</li>
+ <li>Il tuo nome corrente verrà sostituito e perso</li>
+ <li>Se sceglierai un nome corto offensivo (in qualsiasi lingua) potrebbe venir cancellato senza alcun rimborso.</li>
+ <li>'.XAT_NAME.' si riserva il diritto di cancellare e rimborsare qualsiasi nome senza avviso.</li>
+ <li>Un nome corto vi posizionerà più in alto nella lista utenti.</li>',
+     'buypow' => 'Compra i poteri',
+     'maxbuy' => 'Il massimo che puoi comprare è',
+     'notyet' => 'Non puoi comprare più copie di questo potere, riprova tra qualche minuto',
+     'allgone' => 'Ci dispiace, nessun potere rimanente.',
+     'nodays' => 'Non hai abbastanza days',
+     'nothing' => 'Non hai selezionato niente da comprare!',
+     'relogin' => 'Controllo di sicurezza fallito. Clicca <a href="//'.DOMAIN.'/login">qui per effettuare nuovamente il login su '.XAT_NAME.'',
+     'alreadyhave' => 'Possiedi già questi poteri:',
+     'completeta' => 'Acquisto completato. Grazie.',
+     'canpowers' => 'Puoi utilizzare le tue xats per comprare poteri e abilità speciali per la chat.',
+     'readwell' => 'Descrizione <font color="#FF0000">(si prega di leggere attentamente)</font>',
+     'canplay' => 'Con questi poteri potrai avviare e controllare i giochi.',
+     'chkforall' => 'Controlla i poteri che ti servono per allpowers',
+     'unavailable' => 'sarà temporaneamente non disponibile quando esaurito',
+     'remaining' => 'rimanenti!',
+     'willdrop' => 'IL PREZZO SCENDERÀ!',
+     'sold' => 'venduto',
+     'trytrading' => 'Non disponibile, <a href="//'.DOMAIN.'/web_gear/chat/chats.php?type=trade">scambialo</a>',
+     'atauction' => 'Acquistalo all’<a href="//'.DOMAIN.'/web_gear/chat/Auction4.php">asta</a>
+ <li><strong>I poteri funzionano solo se sei abbonato (possiedi days)</strong></li>
+ <li>Più poteri verranno aggiunti in seguito</li>
+ <li>I poteri potrebbero essere ritirati dalla vendita (quindi affrettati e prendi il tuo adesso!)</li>
+ <li>I prezzi dei poteri saliranno e scenderanno a seconda dell’esigenza</li>
+ <li>Gli altri utenti possono vedere quali poteri hai attivato</li>
+ <li>I poteri possono essere attivati e disattivati individualmente</li>
+ <li>I poteri possono essere scambiati con altri utenti</li>
+ <li>I poteri restano tuoi finchè non li scambi</li>
+ <li>Ogni potere possiede una smiley. Solo gli utenti con quel potere potranno usare la smiley.</li>
+ <li><strong>I poteri non possono essere rimborsati. Si prega di leggere la descrizione prima di acquistare.</strong></li>',
+     'power' => 'Potere',
+     'smilie' => 'Smile',
+     'cost' => 'Prezzo',
+     'quantity' => 'Quantità',
+     'orcheck' => 'o spunta',
+     'games' => 'Giochi',
+     'powers' => 'Poteri',
+     'allpowers' => 'AllPowers',
+     'alldesc' => 'Per ottenere allpowers è necessario possedere una copia dei poteri <b>disponibili</b> e standard (contrassegnati da un razzo). I poteri limitati in quantità e i poteri epici non sono richiesti.',
+     'everypower' => 'EveryPower',
+     'everydesc' => 'Per ottenere EveryPower è necessario possedere una copia di tutti i poteri inclusi i rari, i poteri di gruppo e gli epici. Si potrà usare l’omino Rubino. Non potrai scambiare EveryPower.',
+     'epicdesc' => '<b>I Poteri Epici</b><br> sono rari perché sono molti costosi, una parte considerevole del loro fascino deriva dalla loro rarità. Si tratta di un premio per i migliori commercianti che hanno fatto la loro fortuna e un obiettivo da raggiungere per gli altri.',
+     'needlogin' => '(Esegui il <a href="//'.DOMAIN.'/Login">login</a> per cambiarlo)',
+     'markallp' => 'Segna AllPowers',
+     'buyxavis' => 'Compra gli oggetti per xavi',
+     'check' => 'Spunta per comprare',
+     'hat' => 'cappello',
+     'hair' => 'capelli',
+     'xavirules' => '<li>Devi <a href="//'.DOMAIN.'/terms">rispettare i termini</a></li>
+ <li><strong>xavi funziona solo se sei abbonato (possiedi days) e possiedi il power xavi</strong></li>
+ <li>gli oggetti di xavi non si possono scambiare con altri utenti</li>',
+     'type' => 'Tipo',
+     'buygroup' => 'Compra una chat',
+     'gtxnotavail' => 'Trasferimento del gruppo non disponibile',
+     'groupnoexist' => 'Il gruppo non esiste',
+     'notdormant' => 'gruppo non inattivo',
+     'txgroupdone' => 'Trasferimento completato.
+ Si prega di <a href="//'.DOMAIN.'/$RoomName">andare nel gruppo</a>,
+ ricevi una nuova password sulla tua email, e impostala a tuo piacimento.',
+     'youcangroup' => 'Puoi usare le tue xats per trasferire un gruppo che pensi non sia utilizzato.',
+     'groupname' => 'Nome della chat:',
+     'costxats' => 'Costo in xats',
+     'language' => 'Lingua',
+     'editgrp' => 'Modifica la tua chat per cambiarlo',
+     'rulesgroup' => '<li>Devi <a href="//'.DOMAIN.'/terms">rispettare i termini</a></li>
+ <li>I prezzi dei trasferimenti dei gruppi può variare, premi ’Ottieni il costo’ per scoprirlo</li>
+ <li>Se il gruppo diventa inattivo può essere trasferito di nuovo</li>
+ <li>Come email del gruppo verrà utilizzata la stessa del tuo account</li>
+ <li>Se il nome del gruppo è inappropriato (vedi i <a href="//'.DOMAIN.'/terms">termini</a>) il gruppo potrebbe venire cancellato senza rimborso o avviso</li>
+ <li>Non preoccuparti, il tuo gruppo deve essere senza proprietario per più di 3 mesi per venire trasferito</li>',
+     'promo' => 'Promuovi una chat',
+     'langnotset' => 'La lingua del gruppo non è impostata. Si prega di modificare il gruppo per impostarla.',
+     'tinypiconly' => 'L’immagine deve essere su tinypic.com in formato png, jpg o gif non animata',
+     'promona' => 'Ci dispiace, non è possibile promuovere questo gruppo',
+     'promoblocked' => 'Ci dispiace, la promozione per questo gruppo è bloccata per',
+     'delisted' => 'La chat non è in lista. Mettila in lista. (Modifica Chat, Altre Caratteristiche)',
+     'membersonly' => 'La chat è impostata per soli membri. Cambia l’impostazione. (Modifica Chat, Altre Caratteristiche)',
+     'notzero' => 'Non puoi impostare il tempo a zero!',
+     'adssoon' => '<p>Grazie. L’annuncio partirà presto!</p>',
+     'promosoon' => '<p>Grazie! La promozione della chat inizierà presto!</p>',
+     'emailauth' => '<p>Riceverete un’email quando il vostro banner verrà approvato. Grazie.</p>',
+     'canpromo' => 'Puoi utilizzare i tuoi xats per promuovere la tua chat qui.',
+     'promonow' => 'Promuovi adesso',
+     'orads' => 'Pubblicizza la tua chat con un banner',
+     'imageerror' => 'Errore nel controllo dell’immagine. Caricala di nuovo?',
+     'noanim' => 'Le animazioni non sono ammesse per ora, ci dispiace.',
+     'imageinval' => 'Immagine non valida (2)',
+     'imagetype' => 'Tipo di immagine non valido: $type',
+     'imagebig' => 'L’immagine è troppo grande (max $t bytes)',
+     'imagesize' => 'L’immagine deve essere 728 x 90',
+     'days' => 'giorni',
+     'rulespromo' => '<li>Tutti gli aspetti della chat devono essere adatti per l’età di 14 anni e oltre.</li>
+ <li>La chat deve <a href="//'.DOMAIN.'/terms">rispettare i termini</a></li>
+ <li>La chat deve essere moderata durante la promozione(no linguaggi scorretti ecc)</li>
+ <li>CHAT RADIO: Se la radio della chat contiene canzoni con parole inappropriate lo scroller deve includere ATTENZIONE La radio può essere +18 (nella lingua della promozione) o nello sfondo appena sopra lo scroller. I DJ non devono essere inappropriati.</li>
+ <li>Se un utente fa dire al bot qualcosa di inappropriato che è come se lo dicessero loro ,dovrebbe essere bannato.</li>
+ <li>Visitatori inappropriati dovrebbero venire bannati subito (non kickati).</li>
+ <li>Non ci devono essere link inappropriati o links a binari (es programmi)</li>
+ <li>Le chat in promozione NON devono vendere gradi es: vendere il grado di moderatore</li>
+ <li>Le chat NON devono essere basate su argomenti religiosi o politici</li>
+ <li>Almeno un titolare o moderatore deve essere <em>presente<em> a tutte le ore</li>
+ <li>La chat deve essere nella lingua corretta</li>
+ <li>Altre chat possono essere promosse allo stesso tempo della tua</li>
+ <li>I prezzi dei trasferimenti dei gruppi possono variare, premi ’Ottieni il costo’ per scoprirlo</li>
+ <li>La chat deve essere aperta a tutti (non impostata a soli membri, modalità live, ecc). Non bannare e kickare senza un motivo es. nuovi utenti (con o senza id).</li>
+ <li>Il banner della chat verrà mostrato solamente agli utenti che parlano la tua stessa lingua</li>
+ <li>I banner delle chat ora ruoteranno in modo da ottenere una migliore esposizione.</li>
+ <li>Non usare /pr a meno che non sia strettamente necessario. <a href="//util.'.DOMAIN.'/wiki/index.php?title=Commands#protect">Usa /p invece</a> (la chat deve essere aperta a tutti). Puoi perdere la promozione usando /pr.</li>
+ <li>Non sono consentiti banner di vendita di xats a causa delle truffe</li>
+ <li>NEW: Ads must not include ANY items or services that are for sale, this includes but is not limited to xats, days, and powers.</li>
+ <li>Do not promote a chat that is redirected.</li>',
+     'promohours' => 'Ore di promozione',
+     'minhalf' => 'Minimo 0.5 in 1/2 ore',
+     'adimage' => 'Immagine annuncio (728 x 90)',
+     'advertise' => 'Pubblicizza',
+     'aphase' => 'Fase asta',
+     'needmore' => 'Devi offrire più di',
+     'notpart' => 'non fa parte dell’asta!',
+     'ended' => 'L’asta è terminata.',
+     'idblocked' => 'Questo ID è bloccato.',
+     'choose' => 'Scegliere un metodo di trasferimento',
+     'notenough' => 'Non possiedi abbastanza xats/poteri',
+     'alreadybid' => 'Hai già fatto un’offerta',
+     'biddone' => 'Offerta inviata.',
+     'during1' => 'Durante la fase 1 dell’asta puoi suggerire gli ID che desideri acquistare.',
+     'p1ends' => 'La fase 1 finisce tra:',
+     'during2' => 'Durante la fase 2 dell’asta puoi fare un’offerta su tutti gli ID che sono nella lista.',
+     'previous' => 'Risultati delle aste precedenti.',
+     'bids' => 'Offerte attuali:',
+     'idfor' => 'ID in vendita',
+     'timeleft' => 'tempo rimasto',
+     'prevbid' => 'offerta precedente',
+     'congrats' => 'Congratulazioni per aver vinto l’asta.',
+     'auclogin' => 'Per ottenere il tuo nuovo ID effettua l’accesso con il tuo nome utente',
+     'rereg' => 'IMPORTANTE: se vuoi registrare nuovamente il tuo vecchio ID usa questo link',
+     'aucreg' => 'IMPORTANTE: Per registrare il tuo nuovo ID usa questo link',
+     'bid' => 'Offerta',
+     'topbid' => 'Migliore offerente',
+     'prevhere' => 'I risultati delle aste precedenti si trovano <a href="?old=1">qui</a>.',
+     'canauction' => 'Puoi usare i tuoi xats per fare offerte nelle aste.',
+     'auctionrules' => '<li>Devi <a href="//'.DOMAIN.'/terms">rispettare i termini</a></li>
+ <li>'.XAT_NAME.' si riserva il diritto di cancellare e rimborsare qualsiasi asta senza avviso</li>
+ <li>Il tuo nome utente e l’id verranno mostrati nella tabella se sarai il miglior offerente vincitore. (questa è un’asta pubblica)</li>
+ <li>Le offerte sono obbligatorie. Se non verrai superato sarai costretto a pagare.</li>
+ <li>Se sarai il miglior offerente dovrai possedere abbastanza xats e poteri per coprire l’asta.</li>
+ <li>Devi possedere abbastanza xats e poteri non limitati nel tuo account per poter fare un’offerta.</li>
+ <li>Se sarai il vincitore <strong>dovrai reclamare il tuo ID entro 12 ore</strong> dalla fine dell’asta.</li>
+ <li>Se non riuscirai a completare l’asta potresti venire <strong>penalizzato o cancellato</strong>.</li>
+ <li><span class="style1">NUOVO</span> Se offrirai per errore perderai il 20% della tua offerta ma dovrai informare '.XAT_NAME.' immediatamente</li>
+ <li><span class="style1">NUOVO</span> Se non riuscirai a completare l’asta e sposterai gli xats da qualche altra parte perderai il 100% della tua offerta.</li>
+ <li>'.XAT_NAME.' si riserva il diritto di bloccare gli ID dall’asta.</li>
+ <li>assicurati di controllare la <a href="http://util.'.DOMAIN.'/wiki/index.php/Auction">wiki</a> prima di fare offerte</li>
+ <li>Per le aste dei nomi il nome utente verrà assegnato al tuo ID. <strong>Il tuo vecchio nome utente sarà perso.</strong></li>',
+     'minbid' => 'L’offerta minima di questa asta è:',
+     'desired' => 'ID desiderato',
+     'digits' => 'cifre',
+     'enterbif' => 'Inserisci offerta',
+     'buyxats' => 'Compra xats e days',
+     'intro' => ''.XAT_NAME.' ti permette di comprare poteri e tante altre cose per la chat. Inclusi nell’acquisto riceverai anche "days". I "days" determinano il tempo in cui rimarrai abbonato e sono necessari per far funzionare i poteri e altre funzioni della chat. Decidi quanti xats vuoi comprare dalla lista qui sotto a premi uno dei bottoni corrispondenti. Ricordati di avere il permesso dell’intestatario della carta di credito oppure del proprietario dell’account. Tutti gli acquisti sono soggetti ai nostri <a href="//'.DOMAIN.'/terms">termini e condizioni</a>.',
+     'abouttobuyo' => 'Stai per acquistare xats per l’utente:',
+     'notyou' => 'Se questo non è il tuo ID, NON PROCEDERE!',
+     'cash' => 'Banca - PayByCash',
+     'credcard' => 'Carta di credito',
+     'includes' => 'giorni',
+     'max10' => '* Fino ad un massimo di 10 anni. <br>** Per l’Unione Europea: l’IVA sarà aggiunta all’importo o sarà detratta dalle xats. USD = Dollari USA',
+     'reserve' => 'Skrill, PayPal, la carta di credito e il pagamento tramite telefono Android sono soggetti a <a href="http://util.'.DOMAIN.'/wiki/index.php/xatsReserve">riserva</a>. I pagamenti tramite PayPal dovrebbero appartenere ad un solo ID di '.XAT_NAME.'.',
+     'days2xats' => 'Convertire days in xats',
+     'min10' => 'Minimo 10 giorni',
+     'nodx' => 'Non possiedi abbastanza poteri DX',
+     'sorryyou' => 'Devi essere un utente registrato per',
+     'candx' => 'È possibile convertire alcuni dei tuoi days per xats qui.',
+     'daysto' => 'Days da convertire:',
+     'dxrules' => '<li>Devi <a href="//'.DOMAIN.'/terms">rispettare i termini</a></li>
+ <li><strong>Dovrai utilizzare almeno un DX power per ogni conversione.</strong></li>
+ <li>Potrete convertire fino a 180 days per ogni potere DX</li>
+ <li>Riceverete 13.5 xats a day</li>',
+     'convert' => 'Converti',
+     'notconf' => 'registrazione non confermata',
+     'pleasewait' => 'Attendi',
+     'promonote' => 'Nota: se non rispetterai queste regole la tua promozione finirà senza avviso. Non avverrà nessun rimborso di xats e la tua chat potrebbe venire rimossa dalla lista e bloccata dalle promozioni.',
+     'held' => 'Account bloccato. Spiacenti, non puoi comprare nulla.',
+     'pinbad' => 'Codice PIN troppo corto o lettere non valide',
+     'paypal' => 'Paypal',
+     'oneonly' => 'I pagamenti effettuati con PayPal devono appartenere ad un unico id di '.XAT_NAME.'.',
+     'careful' => '(CONTROLLARE CON ATTENZIONE!)',
+     'otherids' => 'Altri ID su cui puntare',
+     'notavail' => 'Ci dispiace. Non disponibile',
+     'imgneedauth' => 'Le immagini degli annunci devono essere autorizzate <font color="#FF0000">(costo 50 xats)</font><br>
+<b>Consulta la <a href="//util.'.DOMAIN.'/wiki/index.php?title=Special:MyLanguage/Advertise">wiki</a> prima di procedere.</b>',
+     'authorize' => 'Autorizza',
+     'winner' => 'Vincitore',
+     'soldcol' => 'Venduto e reclamato',
+     'p2ends' => 'La fase 2 finisce tra:',
+     'forgotpass' => 'Se hai dimenticato la password inserisci il tuo indirizzo email',
+     'wait' => 'Attendi...',
+     'buy' => 'Compra',
+     'norequests' => 'Nessuna richiesta ricevuta fino ad ora',
+     'pinacc' => 'PIN accettato. Riceverai presto una email all’e-mail del tuo account',
+     'refreshok' => 'Aggiornamento riuscito. <br> Chiudi tutte le finestre delle chat.',
+     'refreshRfail' => 'Aggiornamento fallito. Effettua nuovamente il <a href="//'.DOMAIN.'/login">login</a>',
+     'refreshfail' => 'Aggiornamento fallito. Effettua nuovamente il <a href="//'.DOMAIN.'/login">login',
+     'xats2days' => 'Converti xats in days',
+     'canx2d' => 'Puoi convertire un po’ dei tuoi xats in days qui.',
+     'x2drules' => '<li>Devi <a href="//'.DOMAIN.'/terms.html">rispettare ed accettare i termini</a></li>
+ <li><strong>Dovrai utilizzare almeno un power X2D per ogni conversione.</strong></li>
+ <li>Puoi convertire fino a 2500 xats per ogni potere X2D</li>
+ <li>Otterrai 1 day per 13.5 xats</li>
+ </ul>',
+     'xatsto' => 'xats da convertire:',
+     'min100' => 'Minimo 100 xats',
+     'nox2d' => 'Non possiedi abbastanza poteri X2D',
+     'converted' => 'Convertito',
+     'already' => 'è già in promozione. Attendi che il tempo della promozione finisca prima di promuoverla di nuovo.',
+     'ios' => 'Compra xats su un dispositivo Apple',
+     'android' => 'Compra xats su un dispositivo Android',
+     'buythanks' => 'Grazie per il tuo acquisto. <p>Il tuo ordine verrà processato a breve e riceverai una mail di conferma. Se non ricevi la mail di conferma entro 30 minuti, prova a fare nuovamente il login: <a href="//'.DOMAIN.'/login">https://'.DOMAIN.'/login</a></p>'
+	 ]
+?>
