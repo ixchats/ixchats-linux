1e3731820acfad07ffaa1c9581aab93a25832681
ErrorOliver <ErrorOliver6@proton.me>
2025-09-06 05:33:00 +0000

    Create auth.blade.php

---
+++ app/views/layout/panel/src/footer/auth.blade.php
@@ -0,0 +1,10 @@
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="/assets/panel/vendors/js/vendor.bundle.base.js"></script>
+  <script src="/assets/panel/vendors/js/vendor.bundle.addons.js"></script>
+  <!-- endinject -->
+  <!-- inject:js -->
+  <script src="/assets/panel/js/template.js"></script>
+  <!-- endinject -->
+</body>
+</html>
