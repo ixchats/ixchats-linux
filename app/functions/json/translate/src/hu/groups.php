0beb98fcef8509cf279cc8aedfbe167792895b02
ErrorOliver <ErrorOliver6@proton.me>
2025-09-06 03:46:26 +0000

    Add files via upload

---
+++ app/functions/json/translate/src/hu/groups.php
@@ -0,0 +1,12 @@
+<?php
+$array = [
+     'xatgroups' => ''.XAT_NAME.' chatek',
+     'searchb' => 'Keresés',
+     'featured' => 'Kiemelt',
+     'popular' => 'Népszerű',
+     'supported' => 'Támogatott',
+     'games' => 'Játékok',
+     'prev' => 'Előző',
+     'next' => 'Következő'
+	 ]
+?>
---
+++ app/functions/json/translate/src/hu/index.php
@@ -0,0 +1,26 @@
+<?php
+$array = [
+     'notrun' => 'FIGYELEM: Ez a chat nem a '.XAT_NAME.' által van futtatva.',
+     'promotedgroups' => 'Promotált chatek',
+     'new' => 'Új vagy a '.XAT_NAME.'nál?',
+     'newt' => 'Regisztrálj egy fiókot. Keress olyan chateket amelyekben hasonló érdeklődési körü embereket találhatsz. Vásárolj és kereskedj powereiddel, valamint játsz többjátékos játékokat.',
+     'newb' => 'Tudj meg többet',
+     'powers' => 'Powerek',
+     'powerst' => 'Használhatja a xats és napok vásárolni különleges hatáskörrel a '.XAT_NAME.' mezőbe. Ezek közé tartoznak a készletek az egyéni hangulatjelek, speciális effektek és képességek és a csoport hatásköröket extra funkciók a '.XAT_NAME.' csoporthoz. Tudjon meg többet a rendelkezésre álló hatáskörök a <a href="http://util.'.DOMAIN.'/wiki/index.php/Powers">'.XAT_NAME.' wiki</a> .',
+     'powersb' => 'Powerek Vásárlása',
+     'games' => 'Játékok',
+     'gamest' => 'Játsz játékokat idegenekkel vagy a barátaiddal. <br> Játsz, hogy te legyél a leggyorsabb a verseny játékokban vagy a tiltási játékokban. <a href="http://'.DOMAIN.'/game?ss=10000">Doodle</a> - Egy ügyességi rajzolós alkalmazás. <a href="http://'.DOMAIN.'/game?ss=60189">DoodleRace</a>Ügyességi rajzolós játék.Próbáld ki az új <a href="http://'.DOMAIN.'/game?ss=60225">Heart</a> kártyajátékot.',
+     'gamesb' => 'Játsz most',
+     'xavi' => 'Xavi animált avatarok',
+     'xavit' => 'Tervezd meg az egyéni animált karaktered a xavi alkalmazással és nézd ahogy válaszol arra, amit írsz. Egyéni xavi termékeket a <a href="http://'.DOMAIN.'/web_gear/chat/Getxavis.php">xavi boltban</a> <small>vásárolhatsz. (xavi power szükséges.)</small>',
+     'xavib' => 'Készítsd el a saját xavidat.',
+     'group' => 'Létrehozása '.XAT_NAME.' csoport',
+     'groupt' => 'A saját chated kapcsolódhat egy zenei stílushoz, animéhez, videókhoz, játékokhoz stb. , lehet bárki számára elérhető , vagy csak te és a barátaid számára.A chat beálltítások megengedik, hogy csak tagok használhassák a chatedet.',
+     'groupb' => ''.XAT_NAME.' chat létrehozás',
+     'trade' => 'Powerekkel való kereskedés',
+     'tradet' => 'Lehetőséged van kereskedni a powereikkel, eladni azkat más '.XAT_NAME.' felhasználüknak. Ha van egy kis ügyességed, profittal zárhatod üzleteidet.<a href="http://'.DOMAIN.'/web_gear/chat/chats.php?type=trade">'.XAT_NAME.' Kereskedelem</a> a hivatalos hely, ahol vásálók és a vevők találkoznak.',
+     'tradeb' => 'Kereskedés elkezdése',
+     'promomine' => 'Saját chatem promotálása',
+     'follow' => 'Kövesd a '.XAT_NAME.'ot a twitteren.'
+	 ]
+?>
