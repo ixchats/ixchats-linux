f470ddb50437498490455d25026743aa4adf2504
ErrorOliver <ErrorOliver6@proton.me>
2025-09-06 05:35:23 +0000

    Add files via upload

---
+++ app/views/layout/panel/src/footer/foot.php
@@ -0,0 +1,11 @@
+ </div><!-- container -->
+    </div><!-- slim-mainpanel -->
+
+		<div class="slim-footer">
+		  <div class="container">
+			<p>Copyright <?php echo date('Y')?> &copy; All Rights Reserved. <?php echo XAT_NAME?></p>
+			<p>Designed by: <a href="">Ace and ErrorOliver</a></p>
+		  </div><!-- container -->
+		</div><!-- slim-footer -->               
+    </body>
+</html>
