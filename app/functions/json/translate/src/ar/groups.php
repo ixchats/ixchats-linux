cf29db4bfafe923631abb184d5350676b7656005
ErrorOliver <ErrorOliver6@proton.me>
2025-09-06 03:38:40 +0000

    Add files via upload

---
+++ app/functions/json/translate/src/ar/groups.php
@@ -0,0 +1,12 @@
+<?php
+$array = [
+     'xatgroups' => ''.XAT_NAME.' مجموعات',
+     'searchb' => 'بحث',
+     'featured' => 'ألمتميزه',
+     'popular' => 'الشعبيه',
+     'supported' => 'المدعومه',
+     'games' => 'ألعاب',
+     'prev' => 'السابق',
+     'next' => 'التالي'
+	 ]
+?>
