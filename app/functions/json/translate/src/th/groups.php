380214a969ea8f37eca46e30c343606480996830
ErrorOliver <ErrorOliver6@proton.me>
2025-09-06 03:50:30 +0000

    Add files via upload

---
+++ app/functions/json/translate/src/th/groups.php
@@ -0,0 +1,12 @@
+<?php
+$array = [
+     'xatgroups' => 'กรุ๊ป '.XAT_NAME.'',
+     'searchb' => 'ค้นหา',
+     'featured' => 'ที่กำลังเด่น',
+     'popular' => 'เป็นที่นิยม',
+     'supported' => 'สนับสนุน',
+     'games' => 'เกมส์',
+     'prev' => 'ก่อนนี้',
+     'next' => 'ถัดไป'
+	 ]
+?>
