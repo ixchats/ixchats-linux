74a4b4a36c7b755c76eabf9bc64b2055a95f6884
ErrorOliver <ErrorOliver6@proton.me>
2025-09-06 03:51:55 +0000

    Create buy.php

---
+++ app/functions/json/translate/src/tr/buy.php
@@ -0,0 +1,251 @@
+<?php
+$array = [
+     'buyshort' => 'kisa kullanici ismi satin al',
+     'notavailable' => 'Üzgünüm. Kisa isim mevcut degil',
+     'terms' => 'Sartlari kabul etmelisiniz',
+     'nametaken' => 'Üzgünüm, isim zaten alinmis',
+     'costchanged' => 'Maliyet degisti',
+     'notfound' => 'bulunamadi',
+     'wrongpassword' => 'Yanlis sifre',
+     'noxats' => 'Yeterli xats yok',
+     'namechanged' => 'Isim degistirildi',
+     'xatname' => ''.XAT_NAME.' kullanici ismi:',
+     'password' => 'sifre',
+     'desiredname' => 'tercih edilen Isim:',
+     'noteil' => 'Not: ’i’ her zaman kücük harf. ’L’ her zaman büyük harf olur',
+     'iagree' => 'Kurallari ve <a href="//'.DOMAIN.'/terms">sartlari</a> okudum kabul etmis bulunuyorum',
+     'getcost' => 'fiyati al',
+     'once' => 'Sadece bir kez tikla!',
+     'letters' => 'Isim uygunsuz harf iceriyor',
+     'short' => 'Isim cok kisa',
+     'long' => 'Isim cok uzun',
+     'notallowed' => 'Isme izin verilmiyor. Lütfen baska deneyin.',
+     'firstletter' => 'Ilk harf sayi olamaz.',
+     'youcan' => 'Kisa bir kullanici ismi satin almak icin xats’larinizi kullanabilirsiniz (4-9 harf arasi).',
+     'rules' => 'kurallar:',
+     'rulesname' => '<li><a href="//'.DOMAIN.'/terms">Sartlara uymak</a> zorundasin</li>
+ <li>Kisa isim maliyeti degisir, ögrenmek icin Fiyat al’a basin</li>
+ <li>Eger kisa ismin 90 gün kullanilmazsa tekrar satisa sunulabilir</li>
+ <li>Kisi basina sadece bir kisa isme izin verilir</li>
+ <li>Gecerli isminiz degistirildi ve kaybolur</li>
+ <li>Eger sectigin kisa isim (herhangi bir dilde) hakaret iceriyorsa geri ödeme yapilmaksizin silinir</li>
+ <li>'.XAT_NAME.' bildirimde bulunmadan herhangi bir ismi iptal ve iade etme hakkini sakli tutar</li>
+ <li>Not: kisa isim sizi kullanici listesinde üst siralara tasir</li>',
+     'buypow' => 'güc al',
+     'maxbuy' => 'Maksimum satin alabilecegin',
+     'notyet' => 'Suan daha fazla güc alamazsiniz, lütfen bir kac dakika sonra tekrar deneyin',
+     'allgone' => 'Üzgünüm. Güc kalmadi.',
+     'nodays' => 'Yeterli days yok',
+     'nothing' => 'Satin alacak bir sey yok!',
+     'relogin' => 'Güvenlik kontrolü basarisiz. <a href="//'.DOMAIN.'/login">'.XAT_NAME.' chate tekrar giris yapmak icin buraya</a> tikla.',
+     'alreadyhave' => 'Zaten bu güclere sahipsiniz:',
+     'completeta' => 'Satin alma tamamlandi. Tesekkür ederiz.',
+     'canpowers' => ''.XAT_NAME.' chat icin özel gücler ve yetenekler satin almak icin xatslarinizi kullanabilirsiniz.',
+     'readwell' => 'Aciklama <font color="#FF0000">(Lütfen dikkatli okuyun!)</font>',
+     'canplay' => 'Bu güclerle oyunlari baslatabilir ve kontrol edebilirsiniz.',
+     'chkforall' => 'All powers icin gereken gücleri kontrol et',
+     'unavailable' => 'Satildigi zaman gecici olarak kullanim disi olur',
+     'remaining' => 'Arda kalan!',
+     'willdrop' => 'FIYAT DÜSER!',
+     'sold' => 'satildi',
+     'trytrading' => 'Mevcut degil, <a href="//'.DOMAIN.'/web_gear/chat/chats.php?type=trade">ticareti</a> dene',
+     'atauction' => 'Acik artirmadan satin al <a href="//'.DOMAIN.'/web_gear/chat/Auction4.php">auction</a>
+ <li><strong>Gücler sadece abone oldugunuz zaman calisir (days varsa)</strong></li>
+ <li>Daha fazla güc sonra eklenecek</li>
+ <li>Gücler satisdan cekilebilir (bu yüzden simdi acele et!)</li>
+ <li>Güclerin fiyati talebe bagli olarak düsüs ve cikis yasayarak degisebilir</li>
+ <li>Diger kullanicilar hangi güclerin acik oldugunu görebiliyor</li>
+ <li>Gücler ayri ayri acilip kapatilabilir</li>
+ <li>Gücler diger kullanicilarla ticari yolla alinip satilabilir</li>
+ <li>Gücler onu satmadiginiz takdirde sizde kalir</li>
+ <li>Her gücün bir ifade gücü var. Sadece güce sahip olan kullanicilar ifade gücünü kullanabilir.</li>
+ <li><strong><span class="mw-translate-fuzzy">Lütfen güclerin aciklamalarini okuyun (aciklamayi görmek icin fareyle üzerine gelin)</span></strong></li>',
+     'power' => 'Güc',
+     'smilie' => 'Ifade',
+     'cost' => 'fiyat',
+     'quantity' => 'Miktar',
+     'orcheck' => 'veya kontrol et',
+     'games' => 'Oyunlar',
+     'powers' => 'Gücler',
+     'allpowers' => 'AllPowers',
+     'alldesc' => 'Allpowerse hak kazanmak icin <b>mevcut</b> olan (roketle isaretlenmis) her standart güce sahip olmaniz gerekir. Miktari limitli ve epik gücler gerekli degildir.',
+     'everypower' => 'EveryPower',
+     'everydesc' => 'EveryPowere sahip olmak icin her gücün birine sahip olmalisin, grup ve epik gücleride dagil. Yakut piyonu kullanabilirsin. EveryPower trade yapamazsin.',
+     'epicdesc' => '<b>Epik Gücler</b> <br> cok pahalidir nadir oldugundan dolayi, majörlerin büyük bi orani nadirlikten kaynaklanmaktadir. Onlar sansini kazanmis ve diger kullanicilar icin calisan usta tradeciler icin bir ödüldür.',
+     'needlogin' => '(<a href="//'.DOMAIN.'/Login">login</a> ol bunu degismek icin)',
+     'markallp' => 'All Powers isaretle',
+     'buyxavis' => 'xavi itemleri satin al',
+     'check' => 'satin almak icin kontrol et',
+     'hat' => 'sapka',
+     'hair' => 'sac',
+     'xavirules' => '<li><a href="//'.DOMAIN.'/terms">Sartlara uymak</a> zorundasin</li>
+ <li><strong>xaviler sadece aboneler (days varsa) ve xavi gücüne sahip olanlar icin calisir</strong></li>
+ <li>xavi itemleri diger kullanicilar ile ticarete acik degildir</li>',
+     'type' => 'Tür',
+     'buygroup' => 'sohbet Grubu satin al',
+     'gtxnotavail' => 'Üzgünüm. Grup transferi mevcut degildir',
+     'groupnoexist' => 'Bu grup bulunmuyor',
+     'notdormant' => 'grup acilmiyor',
+     'txgroupdone' => 'Transfer tamamlandi.
+ Lütfen <a href="//'.DOMAIN.'/$RoomName">gruba gidin</a>,
+ e-postaniza yeni bi sifre gönderin, ve hepsini ayarlayin.',
+     'youcangroup' => 'Kullanimda olmayan bir sohbet grubunu transfer etmek icin xatsinizi kullanabilirsiniz.',
+     'groupname' => 'grup ismi:',
+     'costxats' => 'xats miktari',
+     'language' => 'dil',
+     'editgrp' => 'Grubunu degistirmek icin düzenle',
+     'rulesgroup' => '<li><a href="//'.DOMAIN.'/terms">Sartlara uymak</a> zorundasin</li>
+ <li>Grup transfer ücretleri degismis olabilir, ögrenemk icin ’Fiyat al’ basin</li>
+ <li>Eger grup tekrar kullanim disi kalirsa tekrar satisa cikabilir</li>
+ <li>Grubun e-postasi kullanicinin mevcut e-postasina ayarlanir</li>
+ <li>Eger grup ismi uygun degilse (<a href="//'.DOMAIN.'/terms">sartlara</a> bak) bildiri ve geri ödeme yapilmaksizin silinebilir</li>
+ <li>Endiselenmeyin, grubunuz 3 aydan fazla süre sahipsiz kalmadigi takdirde transfer edilmez.</li>',
+     'promo' => 'sohbet grubu tanit',
+     'langnotset' => 'Grup dili ayarli degil. Lütfen dili ayarlamak icin grubu düzenleyin.',
+     'tinypiconly' => 'Resimler tinypic.com png, jpg ya da animasyonsuz gif olmalidir',
+     'promona' => 'Üzgünüm, tanitim mevcut degil',
+     'promoblocked' => 'Üzgünüm, tanitim bloke edilmistir',
+     'delisted' => 'Sohbet liste disidir. Lütfe geri listeye al. (Sohbet düzenle, Ekstra özellikler)',
+     'membersonly' => 'Sohbet sadece üyeler icin. Lütfen degis. (Sohbet düzenle, Ekstra Özellikler)',
+     'notzero' => 'Sifir zamana sahip olamaz!',
+     'adssoon' => '<p>Tesekkür ederiz. Duyuru yakinda baslayacak</p>',
+     'promosoon' => '<p>Tesekkür ederiz. Tanitim yakinda baslayacak!</p>',
+     'emailauth' => '<p>Tesekkür ederiz. Ilaniniz onaylandiktan sonra e-posta ile bildiri alacaksiniz.</p>',
+     'canpromo' => 'Sohbet grubunu burada tanitmak icin xatslarini kullanabilirsin.',
+     'promonow' => 'simdi tanit!',
+     'orads' => ''.XAT_NAME.' üzeri banner reklami araciligi ile sohbetini tanit',
+     'imageerror' => 'Resim denetlenirken hata olustu. Tekrar yükleyin?',
+     'noanim' => 'Üzgünüm, animasyonlara simdilik izin verilmiyor',
+     'imageinval' => 'Gecersiz resim (2)',
+     'imagetype' => 'Gecersiz resim türü: $type',
+     'imagebig' => 'Resim cok büyük (maks $t bayt)',
+     'imagesize' => 'Resim 728 x 90 olmalidir',
+     'days' => 'days',
+     'rulespromo' => '<li>Sohbet tüm yönleri ile 14 yas ve üzeri icin uygun sekilde olmalidir.</li>
+ <li>Sohbet <a href="//'.DOMAIN.'/terms">sartlarina uygun </a> olmalidir</li>
+ <li>Tanitim süresi boyunca sitede yetkili olmak zorundadir (küfür vb seylerin kullanimi yasak)</li>
+ <li>SOHBET RADYO: Eger grup radyosunda küfürlü kelimeler geciyorsa "UYARI Radyo +18" (tanitim yapilan site dilinde) veya sadece alt yazi üstündeki arka planda. Djler küfür kullanmamalidir.</li>
+ <li>Eger bir kullanici bota ayni sekilde uygunsuz kelimeler kullandirirsa, onunda yasaklanmasi gerekir.</li>
+ <li>Uygunsuz davranan ziyaretciler hemen yasaklanmalidir (uyari yapilmaksizin)</li>
+ <li>Uygunsuz ve ikili linkler olmamalidir (örnegin programlar)</li>
+ <li>Tanitimda olan grupta satilik yetki OLMAMALIDIR örnegin: modluk satiyorum</li>
+ <li>Sohbetler dini veya siyasi görüslere dayali OLMAMALI</li>
+ <li>En az bir sahip ya da moderatör <em>mevcut</em> tanitim süresince</li>
+ <li>Sohbet dogru dilde olmalidir</li>
+ <li>Diger sohbetler sizle ayni zamanda tanitim icinde olabilirler</li>
+ <li>Tanitim ücretleri degismis olabilir, ögrenemk icin ’Fiyat al’ basin</li>
+ <li>Sohbet herkese acik olmalidir (sadece üye girisi, canli moda vs ayarli tutmayin).Sebepsiz yasaklama ve sutlama yapmayin örnegin yeni kullanicilari (botlar ya da IDsizleri)</li>
+ <li>Sohbet tanitimlari sadece dilinizi kullanan kullanicilara gösterilir</li>
+ <li>Sohbet tanitimlari artik daha fazla sergi icin dönüstürüldü</li>
+ <li>Cok gerekli degilse /pr kullanmayin. Onun <a href="//util.'.DOMAIN.'/wiki/index.php?title=Commands#protect">yerine /p kullanin</a> (sohbet herkese acik olmali). Tanitimi /pr kullanirsan kaybedebilirsin.</li>
+ <li><span class="mw-translate-fuzzy"><font color="#FF0000">YENI:</font> Dolandiricik sebebiyle artik xats satimina izin verilmiyor</span></li>
+ <li>NEW: Ads must not include ANY items or services that are for sale, this includes but is not limited to xats, days, and powers.</li>
+ <li>Do not promote a chat that is redirected.</li>',
+     'promohours' => 'tanitim saati',
+     'minhalf' => 'Minimum 0.5 1/2 saatdir',
+     'adimage' => 'ilan resmi (728x90)',
+     'advertise' => 'duyuru',
+     'aphase' => 'Acik artirma asamasi',
+     'needmore' => 'Bundan daha fazla teklif vermelisin',
+     'notpart' => 'acik artirmanin parcasi degildir!',
+     'ended' => 'Acik artirma sonlandi.',
+     'idblocked' => 'ID engellendi.',
+     'choose' => 'Lütfen bir transfer metodu secin',
+     'notenough' => 'Yeterli xats/gücler yoktur',
+     'alreadybid' => 'Zaten bir teklif verdin',
+     'biddone' => 'Teklif gönderildi.',
+     'during1' => 'Acik artirmanin 1 asamasinda satin almak istediginiz IDleri önerebilirsiniz.',
+     'p1ends' => 'Asama 1 biter:',
+     'during2' => 'Acik artirmanin 2. asamasinda listede bulunan IDlerden herhangi birine teklif verebilirsiniz.',
+     'previous' => 'Önceki acik artirma sonuclari.',
+     'bids' => 'Mevcut teklifler:',
+     'idfor' => 'Satilik ID',
+     'timeleft' => 'kalan süre',
+     'prevbid' => 'önceki teklif',
+     'congrats' => 'Acik artirmayi kazandiniz tebrik ederiz.',
+     'auclogin' => 'Yeni IDni almak icin isminle giris yap',
+     'rereg' => 'ÖNEMLI: Eger eski IDni yeniden kayit yapmak istersen lütfen bu baglantiyi kullan',
+     'aucreg' => 'ÖNEMLI: Yeni IDni kayit etmek icin lütfen bu baglantiyi kullan',
+     'bid' => 'Teklif',
+     'topbid' => 'En yüksek teklif veren',
+     'prevhere' => 'Önceki acik artirma sonuclari <a href="?old=1">burada</a>.',
+     'canauction' => 'Acik artirmada teklif vermek icin xatslarini kullanabilirsin.',
+     'auctionrules' => '<li><a href="//'.DOMAIN.'/terms">Sartlara uymak</a> zorundasin</li>
+ <li>'.XAT_NAME.' bildirimde bulunmadan iptal ve iade etme hakkini sakli tutar</li>
+ <li>Eger yüksek teklif veren / kazanan iseniz '.XAT_NAME.' isminiz ve kullanici id’niz görülür (bu halka acik bir acik artirmadir)</li>
+ <li>Teklifler baglayicidir. Eger daha yüksek teklif veren yoksa ödemelisiniz.</li>
+ <li>Eger en yüksek telifi verdiyseniz teklifi karsilamak icin hesabinizda yeterli miktarda xats yada güc bulundurmalisiniz.</li>
+ <li>Teklif vermek icin hesabinizda yeterli xats olmali veya limitsiz güc bulundurmamalisiniz.</li>
+ <li>Eger kazanirsan acik artirmanin sonunda <strong>12 saat icerisinde IDni talep etmen gerekir</strong></li>
+ <li>Eger acik artirmayi tamamlayamazsaniz <strong> ceza alabilir ya da silinebilirsiniz </strong></li>
+ <li><span class="style1">YENI</span> Eger yanliskla teklif verirseniz teklifinizin 20% kaybedersiniz ve hemen xate bildirmeniz gerekir.</li>
+ <li><span class="style1">YENI</span> Eger acik artirmayi tamamlamada basarisiz olur ve xatslari uzaklastirirsaniz teklifinizi 100% kaybedersiniz</li>
+ <li>'.XAT_NAME.' acik artirmada bulunan IDleri engelleme hakkini sakli tutar.</li>
+ <li>yani teklif vermeden önce <a href="//util.'.DOMAIN.'/wiki/index.php?title=Special:MyLanguage/Auction">wiki</a> kontrol edin</li>
+ <li>Isim acik artirmalari icin isim IDnize atanir. 
+<strong>Eski ismin kaybolur.</strong></li>',
+     'minbid' => 'Bu acik artirmadaki minimum teklif söyledir:',
+     'desired' => 'Istenen ID',
+     'digits' => 'rakamlar',
+     'enterbif' => 'Teklif gir',
+     'buyxats' => 'xats ve days satin al',
+     'intro' => ''.XAT_NAME.' sohbet sayfaniz icin powers ve daha bir cok sey satin almaniza olanak sagliyor. Ayri satin alimlara bagli olarak "days" de elde etmis oluyorsunuz. Days powersleri kullanmak icin gerekli olan abone süresidir - ve diger sohbet kutusu özelliklerini, calistirmak icin. Asagida bulunan listeden kac xats almak istediginize karar verin ve butonlardan birine tiklayin. Lütfen kartin veya hesap sahibinin izni oldugundan emin olun. Tüm satın alımlar <a href="//'.DOMAIN.'/terms"> kural ve kosullarına tabidir.</a>.',
+     'aboutto' => 'Ilgili kullanici icin xats siparis etmek üzeresiniz:',
+     'notyou' => 'Bu sizin IDniz degilse IZIN VERMEYIN!',
+     'cash' => 'Banka/PayByCash',
+     'credcard' => 'Kredi karti',
+     'includes' => 'days',
+     'max10' => '* maksimum 10 yila kadar. <br>** Avrupa birligi icin: KDV miktari eklenir veya xats’dan düsürülür USD = ABD Dolari',
+     'reserve' => 'Skrill, PayPal, Kredi karti ve Android ödemeleri <a href="//util.'.DOMAIN.'/wiki/index.php?title=Special:MyLanguage/Reserve">transfer kisitlamalarina</a> yol acar. PayPal ödemeleri sadece bir '.XAT_NAME.' ID’si icin olmalidir.',
+     'days2xats' => 'Daysi xatsa dönüstür',
+     'min10' => 'Minimum 10 days',
+     'nodx' => 'Yeterli DX gücü yok',
+     'sorryyou' => 'Üzgünüm buraya kayitli kullanici olmalisin',
+     'candx' => 'Buradan bazi dayslerini xats’e cevirebilirsin.',
+     'daysto' => 'Dönüstürülecek Days:',
+     'dxrules' => '<li><a href="//'.DOMAIN.'/terms">Sartlara uymak</a> zorundasin</li>
+ <li><strong>Her dönüsüm icin bir DX gücü kullanmaniz gerekecek.</strong></li>
+ <li>DX gücü basina 180 dayse kadar dönüstürebilirsiniz</li>
+ <li>Day basina 13.5 xats alirsiniz</li>',
+     'convert' => 'Dönüstür',
+     'notconf' => 'Kayit teyit edilmedi',
+     'pleasewait' => 'Lütfen bekle',
+     'promonote' => 'Aklinizda bulundurun bu kurallara uymadiginiz takdirde bildiri ve xats iadesi yapilmaksizin grubunuz tanitimdan kaldirilir ve liste disina alinir devam tanitima girmeside engellenir.',
+     'held' => 'Hesap durdurmasi. Üzgünüz, satin alamazsiniz.',
+     'pinbad' => 'Pin kodu cok kisa, veya kötü harfler',
+     'paypal' => 'Paypal',
+     'oneonly' => 'PayPal ödemeleri sadece bir '.XAT_NAME.' id üzerinden yapilmali.',
+     'careful' => '(DIKKATLICE KONTROL ET!)',
+     'otherids' => 'Diger teklif verilen IDler',
+     'notavail' => 'Üzgünüm. Mevcut degil',
+     'imgneedauth' => 'Tanitim resimleri izinden gecmesi gerekir <font color="#FF0000">(fiyati 50 xatsdir)</font><br>
+<b>Izin vermeden önce <a href="//util.'.DOMAIN.'/wiki/index.php?title=Special:MyLanguage/Advertise">wiki</a> göz atin.</b>',
+     'authorize' => 'onaylama',
+     'winner' => 'Kazanan',
+     'soldcol' => 'Satildi ve toplandi',
+     'p2ends' => 'Asama 2 biter:',
+     'forgotpass' => 'Eger sifreni unuttuysan e-postani buraya gir.',
+     'wait' => 'Lütfen bekle..',
+     'buy' => 'satin al',
+     'norequests' => 'Simdiye kadar hic bir talep gelmedi',
+     'pinacc' => 'PIN kabul edildi. Kisa sürede kayitli kullanici icin e-posta gönderilecek',
+     'refreshok' => 'Yenileme basarili. <br> Lütfen tüm chat sekmelerini kapatin.',
+     'refreshRfail' => 'Yenileme basarisiz oldu.. Lütfen tekrar <a href="//'.DOMAIN.'/login">login</a> olun.',
+     'refreshfail' => 'Yenileme basarisiz oldu.. Lütfen tekrar <a href="//'.DOMAIN.'/login">login</a> olun.',
+     'xats2days' => 'xats’lari days’e dönüstür',
+     'canx2d' => 'Buradan bazi xats’lerinizi days’e cevirebilirsin.',
+     'x2drules' => '<li><a href="//'.DOMAIN.'/terms.html">Sartlara uymak</a> zorundasin</li>
+ <li><strong>Her dönüsüm icin bir X2D gücü kullanmaniz gerekecek.</strong></li>
+ <li>X2D gücü basina 2500 xats’e kadar dönüstürebilirsiniz</li>
+ <li>13.5 xats basina 1 day alirsin</li>
+ </ul>',
+     'xatsto' => 'dönüstürülecek xats:',
+     'min100' => 'Minimum 100 xats',
+     'nox2d' => 'Yeterli X2D gücü yok',
+     'converted' => 'Dönüstürüldü',
+     'already' => 'zaten tanitimda. Lütfen tanitimdan önce promosyonun bitmesini bekleyin.',
+     'ios' => 'Apple cihazilarindan xats satin alin',
+     'android' => 'Android cihazlarindan xats satin alin',
+     'buythanks' => 'Satin aldigin icin tesekkür ederiz. <p>Talebin yakinda isleme girecek ve sana bir onay e-postasi yollanacaktir. Eger 30 dakika sonra onaylama e-postasini almazsan, buradan giris yapmayi dene: <a href="//'.DOMAIN.'/login">https://'.DOMAIN.'/login</a></p>'
+	 ]
+?>
