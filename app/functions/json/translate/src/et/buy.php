db3b1182c6b5378a3e1cc58e982da9370f8edf12
ErrorOliver <ErrorOliver6@proton.me>
2025-09-06 03:44:06 +0000

    Create buy.php

---
+++ app/functions/json/translate/src/et/buy.php
@@ -0,0 +1,250 @@
+<?php
+$array = [
+     'buyshort' => '<span class="mw-translate-fuzzy">Osta lühike kasutajanimi</span>',
+     'notavailable' => 'Vabandame. Lühinimi pole saadaval',
+     'terms' => 'Pead kasutustingimustega nõustuma',
+     'nametaken' => 'Vabandame, nimi juba kasutusel',
+     'costchanged' => 'Hind muutus',
+     'notfound' => 'Ei leitud',
+     'wrongpassword' => 'Vale parool',
+     'noxats' => 'Pole piisavalt '.XAT_NAME.'’e',
+     'namechanged' => 'Nimi muudetud',
+     'xatname' => 'xat’i kasutajanimi:',
+     'password' => '<span class="mw-translate-fuzzy">Parool</span>',
+     'desiredname' => '<span class="mw-translate-fuzzy">Soovitud nimi:</span>',
+     'noteil' => 'Märkus: Täht ’i’ on alati väikselt ning ’L’ alati suurelt',
+     'iagree' => 'Olen lugenud ning nõustun reeglite ja <a href="//'.DOMAIN.'/terms">kasutustingimustega</a>',
+     'getcost' => '<span class="mw-translate-fuzzy">Hangi hind</span>',
+     'once' => 'Vajuta vaid korra!',
+     'letters' => 'Nimi sisaldab keelatud tähemärke',
+     'short' => 'Nimi on liiga lühike',
+     'long' => 'Nimi on liiga pikk',
+     'notallowed' => 'Nimi pole lubatud. Palun proovi mõnda teist.',
+     'firstletter' => 'Nimi ei tohi alata numbriga.',
+     'youcan' => '<span class="mw-translate-fuzzy">xat’ide eest saad soetada lühinime (4-9 tähemärki).</span>',
+     'rules' => '<span class="mw-translate-fuzzy">Reeglid:</span>',
+     'rulesname' => '<li>Pead <a href="//'.DOMAIN.'/terms">kasutustingimustega</a> nõustuma</li>
+ <li>Lühinime väärtus oleneb selle populaarsusest, vajuta nupule ’Maksumus’, et teada saada</li>
+ <li>Kui sa oma lühinime 90 päeva pole kasutanud, siis võidakse see uuesti müüki panna</li>
+ <li>Üks lühinimi kasutaja kohta</li>
+ <li>Sinu praegune kasutajanimi asendatakse</li>
+ <li>Kui sinu lühinimi on ebasobilik (ükskõik, mis keeles) võidakse see kustutada ning '.XAT_NAME.'’e tagasi ei maksta</li>
+ <li>xat’il on õigus hoiatuseta ükskõik mis nime tagasi võtta, makstes kulutatud '.XAT_NAME.'’id tagasi</li>
+ <li>Märkus: lühinimi paneb sind kõrgemale külastajate nimekirjas</li>',
+     'buypow' => '<span class="mw-translate-fuzzy">Osta võimeid</span>',
+     'maxbuy' => 'Maksimaalne palju osta saad on',
+     'notyet' => 'Sa ei saa hetkel seda võimet rohkem soetada, proovi mõne minuti pärast uuesti',
+     'allgone' => 'Vabandame. Antud võimed väljamüüdud.',
+     'nodays' => 'Pole piisavalt tellija päevi',
+     'nothing' => 'Pole midagi osta!',
+     'relogin' => 'Turvalisuse kontroll ebaõnnestus. Uuesti sisselogimiseks <a href="//'.DOMAIN.'/login">vajuta siia</a>',
+     'alreadyhave' => 'Sa juba omad järgnevaid võimeid:',
+     'completeta' => 'Ost sooritatud. Täname.',
+     'canpowers' => 'Kasutades '.XAT_NAME.'’e ning päevi saate osta erivõimeid ning võimalusi jutukas kasutamiseks.',
+     'readwell' => 'Kirjeldus <font color="#FF0000">(Palun lugege hoolikalt!)</font>',
+     'canplay' => 'Antud võimetega saad käivitada ning juhtida mänge.',
+     'chkforall' => 'Kontrolli, mis võimed on kogumikust puudu',
+     'unavailable' => 'on väljamüüdes ajutiselt kättesaadamatu',
+     'remaining' => 'saadaval!',
+     'willdrop' => 'HIND LANGEB!',
+     'sold' => 'müüdud',
+     'trytrading' => 'Ei ole saadaval, proovi <a href="//'.DOMAIN.'/web_gear/chat/chats.php?type=trade">kaubelda</a>',
+     'atauction' => 'Osta <a href="//'.DOMAIN.'/web_gear/chat/Auction4.php">oksjonil</a>
+ <li><strong>Võimed töötavad vaid tellija päevade olemasolul</strong></li>
+ <li>Ajapikku lisatakse võimeid juurde</li>
+ <li>Võimed, mida on piiratud koguses kaovad müügist peagi, seega ole kiire!</li>
+ <li>Sõltuvalt nõudlusest võib võimete hind olla kõikuv</li>
+ <li>Teised kasutajad näevad, mis võimeid sa kasutad</li>
+ <li>Võimeid saab individuaalselt sisse-välja lülitada</li>
+ <li>Võimeid saab teiste kasutajatega vahetada</li>
+ <li>Võimed on sinuga seni, kuni sa neist loobud</li>
+ <li>Igal võimel on sellele omane smail. Ainult vastavat võimet omav kasutaja saab seda smaili kasutada.</li>
+ <li><strong><span class="mw-translate-fuzzy">Palun tutvu kirjeldusega, enne võime ostmist (liikudes hiirega peale kuvab hüpikaknas kirjeldust)</span></strong></li>',
+     'power' => 'Võimed',
+     'smilie' => 'Smail',
+     'cost' => '<span class="mw-translate-fuzzy">Hind</span>',
+     'quantity' => 'Kogus',
+     'orcheck' => 'või tee linnuke',
+     'games' => 'Mängud',
+     'powers' => 'Võimed',
+     'allpowers' => 'AllPowers',
+     'alldesc' => 'Et kvalifitseeruda ’AllPowers’ võime soetamiseks, tuleb sul omandada kõik <b>saadavaloleval</b> standardsed võimed (märgistatud raketiga). Piiratud ning eepiliste võimete olemasolu pole oluline.',
+     'everypower' => 'EveryPower',
+     'everydesc' => 'Et olla kvalifitseeritud ’EveryPower’ võime soetamiseks, tuleb omada kõiki saadavalolevaid võimeid, sealhulgas haruldasi, grupivõimeid ning eepilisi võimeid. Seejärel saad kasutusse rubiinnupu, mida pole võimalik müüa ega vahetada.',
+     'epicdesc' => '<b>Eepilised võimed</b> <br> on oma hinna tõttu haruldased, seega tõmbad sellevõrra rohkem inimeste tähelepanu endale. Need on tunnustamaks meisterkauplejaid, kes on saavutanud rikkuse, milleni inspireeritud saaksid pürgida.',
+     'needlogin' => '(selle muutmiseks <a href="//'.DOMAIN.'/Login">logi sisse</a>)',
+     'markallp' => 'Vali kõik võimed',
+     'buyxavis' => 'Osta xavi esemeid',
+     'check' => 'vali, et osta',
+     'hat' => 'peakate',
+     'hair' => 'juuksed',
+     'xavirules' => '<li>Esmalt pead <a href="//'.DOMAIN.'/terms">nõustuma kasutustingimusega</a></li>
+ <li><strong>xavis töötab vaid siis, kui sul on tellija päevi ning xavis võime</strong></li>
+ <li>xavi esemetega ei saa kaubelda</li>',
+     'type' => 'Tüüp',
+     'buygroup' => '<span class="mw-translate-fuzzy">Osta jutuka grupp</span>',
+     'gtxnotavail' => 'Vabandame. Gruppi pole võimalik omandada',
+     'groupnoexist' => 'Seda gruppi ei eksisteeru',
+     'notdormant' => 'grupp pole ebaaktiivne',
+     'txgroupdone' => 'Grupp ülevõetud.
+ Palun <a href="//'.DOMAIN.'/$RoomName">külasta grupilehte</a>,
+ ning saada parool meilile, et seejärel gruppi seadistada.',
+     'youcangroup' => 'Kasutades '.XAT_NAME.'’e saad ebaaktiivse jutuka üle võtta.',
+     'groupname' => '<span class="mw-translate-fuzzy">Grupi nimi:</span>',
+     'costxats' => 'Maksumus '.XAT_NAME.'’ides',
+     'language' => '<span class="mw-translate-fuzzy">Keel</span>',
+     'editgrp' => 'Redigeeri gruppi, et muuta',
+     'rulesgroup' => '<li>Esmalt pead <a href="//'.DOMAIN.'/terms">nõustuma kasutustingimusega</a></li>
+ <li>Grupi hinnad varieeruvad, vajuta nupule ’Maksumus’, et teada saada</li>
+ <li>Ebaaktiivsed grupid lastakse uuesti kasutusse</li>
+ <li>Grupi meiliaadress määratakse samaks, mis sinu kasutajakontol</li>
+ <li>Kui sinu grupinimi on ropp, ebasobilik (vaata <a href="//'.DOMAIN.'/terms">kasutustingimusi</a>) võidakse see hoiatamata ning hüvitiseta kustutada</li>
+ <li>Kui grupp on 3 kuud omanikuta läheb see uuesti müüki</li>',
+     'promo' => '<span class="mw-translate-fuzzy">Reklaami jutuka gruppi</span>',
+     'langnotset' => 'Grupil pole keel seadistatud. Palun määrake grupi sätetes grupile keel.',
+     'tinypiconly' => 'Pilt peab olema tinypic.com domeenis, lubatud formaatideks png, jpg ning staatiline gif',
+     'promona' => 'Vabandame, reklaamimisvõimalus puudub',
+     'promoblocked' => 'Vabandame, reklaamimine keelatud, põhjusel',
+     'delisted' => 'Jutukas pole avalik (seda ei kuvata nimekirjades). Palun muuda see taas avalikuks jutuka sätetes.',
+     'membersonly' => 'Jutukas on piiratud vaid liikmetele kasutamiseks. Palun eemalda need piirangud jutuka sätetes.',
+     'notzero' => 'Miinimum aeg reklaamimiseks on pool tundi (0.5)!',
+     'adssoon' => '<p>Täname, reklaam hakkab peagi tööle</p>',
+     'promosoon' => '<p>Täname, reklaam läheb peagi üles!</p>',
+     'emailauth' => '<p>Täname, sulle saadetakse e-posti kiri, kui reklaam lubatakse.</p>',
+     'canpromo' => 'Grupi reklaamimiseks tuleb maksta '.XAT_NAME.'’ides.',
+     'promonow' => '<span class="mw-translate-fuzzy">Reklaami nüüd!</span>',
+     'orads' => '<span class="mw-translate-fuzzy">Reklaami oma jututuba, kasutades bännerit</span>',
+     'imageerror' => 'Pildi kontrollimisel tekkis viga. Laadi uuesti üles?',
+     'noanim' => 'Vabandame, animeeritud pildid pole hetkel lubatud',
+     'imageinval' => 'Vigane pilt (2)',
+     'imagetype' => 'Keelatud pildiformaat: $type',
+     'imagebig' => 'Pilt on liiga mahukas (maksimaalselt $t baiti)',
+     'imagesize' => 'Pilt peab olema mõõtmetes 728x90',
+     'days' => 'päevad',
+     'rulespromo' => '<li>Kõik jututoas sisalduv, toimuv peab olema sobilik inimestele, vanuses 14 ja üle</li>
+ <li>Jututuba on kohustatud tegutsema <a href="//'.DOMAIN.'/terms">kasutustingimustest</a> sõltuvalt</li>
+ <li>Jututoas peab igal ajal sees olema mõni meeskonnaliikmetest, et ei toimuks ropendamist ja muud ebasobilikku</li>
+ <li>JUTUTOA RAADIO: Kui jututoa raadios mängitakse lugusid, mis sisaldavad roppust või vandumist, tuleb jututoa kerivasse teksti kaasata vastav hoiatuskiri, stiilis HOIATUS Raadio võib olla alla 18 aastastele mittesobilik (seda keeles, mille all jutukat reklaamitakse) või lisada vastav tekst taustale kohe keriva teksti kohale. Plaadikeerutajad ei tohiks ebasobilikku kõnepruuki kasutada.</li>
+ <li>Kui kasutaja paneb jututoa robotit midagi ebasobilikku ütlema, võrdub see samaga kui kasutaja oleks seda ise öelnud, seega tuleks ta koheselt jutukast eemaldada.</li>
+ <li>Kasutajad, kes ropendavad või väljendavad end muul moel labaselt, ebasobilikult, tuleks koheselt jutukast eemaldada (mitte välja visata)</li>
+ <li>Ebasobiliku materjalini ning programmideni linkimine on keelatud</li>
+ <li>Jutukad, mida reklaamitakse EI TOHI tegeleda positsioonide müügiga (nt moderaatori, omaniku auastmete müük)</li>
+ <li>Jutukas EI TOHI baseeruda usulistel või poliitilistel vaadetel</li>
+ <li>Vähemalt üks moderaator või omanik peab igal ajal <em>nähtavana</em> sees viibima</li>
+ <li>Jutukas peab olema korrektses keeles</li>
+ <li>Samaaegselt võivad teised jututoad ennast reklaamida</li>
+ <li>Reklaami hinnad võivad varieeruda, vajuta nupule ’Maksumus’, et teada saada</li>
+ <li>Jutukas peab olema kõigile avatud, seega enne reklaamimist lülita piirangud välja (kui need on seatud). Ära kedagi ilma põhjuseta jutukast välja viska ega eemalda</li>
+ <li>Jutukat kuvab ainult kasutajatele, kes on seadnud enda keeleks jutukas kasutatava</li>
+ <li>Reklaamitavad jutukad vahetuvad suurema liikluse tabamiseks</li>
+ <li>Väldi käsu /pr kasutamist, kui see pole just hädavajalik. Kasuta selle asemel <a href="//util.'.DOMAIN.'/wiki/index.php?title=Commands#protect">käsku /p</a> (jutukas peab olema kõigile avatud). /pr kasutamise tulemusel võidakse su reklaam eemaldada</li>
+ <li><span class="mw-translate-fuzzy"><font color="#FF0000">UUS:</font> Pettuste tõttu pole lubatud reklaamida '.XAT_NAME.'’ide müüki</span></li>
+ <li>NEW: Ads must not include ANY items or services that are for sale, this includes but is not limited to xats, days, and powers.</li>
+ <li>Do not promote a chat that is redirected.</li>',
+     'promohours' => '<span class="mw-translate-fuzzy">Tunde reklaamimiseks</span>',
+     'minhalf' => 'Pool tundi (0.5) minimaalselt',
+     'adimage' => '<span class="mw-translate-fuzzy">Reklaami pilt</span>',
+     'advertise' => '<span class="mw-translate-fuzzy">Reklaam</span>',
+     'aphase' => 'Oksjoni etapp',
+     'needmore' => 'Pead panustama rohkem kui',
+     'notpart' => 'ei osale oksjonil!',
+     'ended' => 'Oksjon on lõppenud.',
+     'idblocked' => 'See ID on blokeeritud.',
+     'choose' => 'Palun vali ülekande meetod',
+     'notenough' => 'Pole piisavalt '.XAT_NAME.'’e või võimeid',
+     'alreadybid' => 'Oled juba panustanud',
+     'biddone' => 'Panus esitatud.',
+     'during1' => 'Esimese etapi vältel saad soovitada ID-sid, mida sooviksid osta.',
+     'p1ends' => 'Esimene etapp lõpeb:',
+     'during2' => 'Teise etapi vältel võid panustada ükskõik millisele ID-le nimekirjas.',
+     'previous' => 'Eelmise oksjoni tulemused.',
+     'bids' => 'Praegused panused:',
+     'idfor' => 'ID müügiks',
+     'timeleft' => 'aega järgi',
+     'prevbid' => 'Eelmine panus',
+     'congrats' => 'Õnnitlused oksjoni võitmise puhul.',
+     'auclogin' => 'Uue ID omandamiseks logi oma kasutajanimega sisse',
+     'rereg' => 'TÄHTIS: Palun kasuta seda linki, kui soovid oma vana ID uuesti registreerida',
+     'aucreg' => 'TÄHTIS: Kasuta palun seda linki uue ID registreerimiseks',
+     'bid' => 'Panus',
+     'topbid' => 'Suurim panus kasutajalt',
+     'prevhere' => 'Eelmise oksjoni tulemused leiad <a href="?old=1">siit</a>.',
+     'canauction' => 'Oksjonil panustamiseks saad kasutada '.XAT_NAME.'’e.',
+     'auctionrules' => '<li>Esmalt pead <a href="//'.DOMAIN.'/terms">nõustuma kasutustingimusega</a></li>
+ <li>xat’il on õigus oksjon hoiatamata katkestada ning panustatu hüvitada</li>
+ <li>Tegemist on avaliku oksjoniga, seega sinu nimi jääb kõrgeima panuse ning võidu korral kõigile nähtavale</li>
+ <li>Kõrgeima panustajana oled kohustatud maksma</li>
+ <li>Olles kõrgeim panustaja, veendu, et sinu kontol oleks piisavalt '.XAT_NAME.'’e oksjoni lõppedes.</li>
+ <li>Panustamiseks peab sul olema piisavalt '.XAT_NAME.'’e või võimeid kontol (piiratud võimeid ei arvestata).</li>
+ <li>Võites <strong>tuleb ID omandada 12 tunni sees</strong> pärast oksjoni lõppu</li>
+ <li>Panust võidu korral mitte tasudes võidakse su kontot <strong>trahvida või kustutada</strong></li>
+ <li><span class="style1">UUS</span> Panustades kogemata, kaotad 20% panustatust, kuid pead '.XAT_NAME.'’i koheselt teavitama</li>
+ <li><span class="style1">UUS</span> Oksjonit mitte lõpuni viies kaotad 100% panustatust</li>
+ <li>xat’il on õigus keelata konkreetsete ID-de osalemist oksjonil.</li>
+ <li>enne panustamist loe <a href="//util.'.DOMAIN.'/wiki/index.php?title=Special:MyLanguage/Auction">vikist</a></li>
+ <li>Nime oksjonite puhul määratakse nimi oksjonil osalevale kasutajale. <strong>Vanast nimest jääd ilma.</strong></li>',
+     'minbid' => 'Miinimumpanuseks on:',
+     'desired' => 'Soovitud ID',
+     'digits' => 'kohaline',
+     'enterbif' => 'Sisesta panus',
+     'buyxats' => 'Osta '.XAT_NAME.'’e ning tellija päevi',
+     'intro' => 'xats allow you to buy powers and many other things for the chat. You also get "days" that are included with purchases. Days are subscriber time that are needed to make powers - and other chat box features - work. Decide how many xats you would like to purchase from the list below and press one of the buttons. Please ensure that you have permission of the card or account holder. All purchases are subject to our <a href="//'.DOMAIN.'/terms">terms and conditions</a>.',
+     'aboutto' => 'Olete tellimas '.XAT_NAME.'’e ning tellija päevi kasutajale:',
+     'notyou' => 'Kui see pole sinu ID, siis ÄRA JÄTKA!',
+     'cash' => 'Pank/PayByCash',
+     'credcard' => 'Krediitkaart',
+     'includes' => 'päevad',
+     'max10' => '* maksimaalselt 10 aastat. <br>** Euroopa Liidus on käibemaks summale juurde lisatud või '.XAT_NAME.'’idelt maha arvestatud USD = Ameerika Ühendriikide dollarid',
+     'reserve' => '<span class="mw-translate-fuzzy">PayPal-i, krediitkaardi ning telefonimaksetest saab lähemalt lugeda <a href="//util.'.DOMAIN.'/wiki/index.php?title=Special:MyLanguage/Reserve">vikist</a>. PayPal-i makseid tuleks sooritada vaid ühele ID-le.</span>',
+     'days2xats' => 'Muuda päevad '.XAT_NAME.'’ideks',
+     'min10' => 'Minimaalselt 10 tellija päeva',
+     'nodx' => 'Pole piisavalt DX võimeid',
+     'sorryyou' => 'Vabandame, peate omama registreeritud kontot',
+     'candx' => 'Siin saad teisendada osa või kõik oma päevadest '.XAT_NAME.'’ideks.',
+     'daysto' => 'Päevi teisendamisele:',
+     'dxrules' => '<li>Esmalt pead <a href="//'.DOMAIN.'/terms">nõustuma kasutustingimusega</a></li>
+ <li><strong>Iga teisendus eeldab DX võime olemasolu</strong></li>
+ <li>Üks DX võime kaob 180 päeva teisendamisel</li>
+ <li>Üks päev annab 13.5 '.XAT_NAME.'’i</li>',
+     'convert' => 'Teisenda',
+     'notconf' => 'registreeritud konto kinnitamata',
+     'pleasewait' => 'Palun oota',
+     'promonote' => 'Märkus: kui sa ei järgi ülalolevaid reegleid võetakse su reklaam etteteatamata maha, kulutatud '.XAT_NAME.'’e ei hüvitata ning edaspidised reklaamimised keelatakse.',
+     'held' => '<span class="mw-translate-fuzzy">Konto ootel. Ostu ei saa sooritada.</span>',
+     'pinbad' => 'PIN kood on kas liiga lühike või sisaldab keelatud tähemärke',
+     'paypal' => 'PayPal',
+     'oneonly' => 'PayPal-i maksed tuleks teostada vaid ühe konto ID-le.',
+     'careful' => '(KONTROLLI HOOLIKALT!)',
+     'otherids' => 'Teised ID-d millele panustatud',
+     'notavail' => 'Vabandage. Pole saadaval',
+     'imgneedauth' => 'Piltide kasutus tuleb esmalt autoriseerida <font color="#FF0000">(maksumus 50 '.XAT_NAME.'’i)</font><br>
+<b>Enne jätkamist uuri <a href="//util.'.DOMAIN.'/wiki/index.php?title=Special:MyLanguage/Advertise">vikist</a> lähemalt.</b>',
+     'authorize' => '<span class="mw-translate-fuzzy">Autoriseeri</span>',
+     'winner' => 'Võitja',
+     'soldcol' => 'Müüdud ning omandatud',
+     'p2ends' => 'Teine etapp lõpeb:',
+     'forgotpass' => 'Sisesta siin oma meiliaadress, kui oled parooli unustanud',
+     'wait' => 'Palun oota..',
+     'buy' => '<span class="mw-translate-fuzzy">Osta</span>',
+     'norequests' => 'Seni pole päringuid esitatud',
+     'pinacc' => 'PIN aksepteeritud. Peagi saadetakse kasutaja meiliaadressile kiri edasiste juhistega',
+     'refreshok' => 'Lehevärskendus edukas. <br> Palun sulge kõik jutuka aknad.',
+     'refreshRfail' => 'Lehevärskendus ebaõnnestus.. Palun <a href="//'.DOMAIN.'/login">logi uuesti sisse</a>.',
+     'refreshfail' => 'Lehevärskendus ebaõnnestus.. Palun <a href="//'.DOMAIN.'/login">logi uuesti sisse</a>.',
+     'xats2days' => 'Muuda '.XAT_NAME.'’id tellija päevadeks',
+     'canx2d' => 'Saad '.XAT_NAME.'’id tellija päevadeks ümber teisendada.',
+     'x2drules' => '<li>Esmalt pead <a href="//'.DOMAIN.'/terms.html">nõustuma kasutustingimusega</a></li>
+ <li><strong>Iga teisendus eeldab X2D võime olemasolu</strong></li>
+ <li>Üks X2D võime kaob 2500 '.XAT_NAME.'’i teisendamisel</li>
+ <li>Makstes 13.5 '.XAT_NAME.'’i saad ühe päeva</li>
+ </ul>',
+     'xatsto' => 'xat’e teisendamisele:',
+     'min100' => 'Minimaalselt 100 '.XAT_NAME.'’i',
+     'nox2d' => 'Pole piisavalt X2D võimeid',
+     'converted' => 'Teisendatud',
+     'already' => 'juba reklaamitakse. Palun oota kuni kestev reklaam saab läbi.',
+     'ios' => 'Osta '.XAT_NAME.'’e Apple seadmel',
+     'android' => 'Osta '.XAT_NAME.'’e Android seadmel',
+     'buythanks' => 'Thank you for your purchase. <p>Your order will be processed soon and you will be sent a confirmation email. If you don’t receive the confirmation email after 30 minutes, try signing in here: <a href="//'.DOMAIN.'/login">https://'.DOMAIN.'/login</a></p>'
+	 ]
+?>
